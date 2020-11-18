b80e10457d901f58a3e0786536bed1c915323b35
Fumiya Sato <fumiya.sato.rop@gmail.com>
2020-11-18 22:03:08 +0900

    mod: meter simulation demo for event was modified a little.

--- event2020/index.js
+++ event2020/index.js
@@ -1,11 +1,18 @@
 const setSeitchTimeThr = 1000;
 var v_set = 100;
+var ig = 0;
 
+(window.onload = function() {
+    document.getElementById("meterDisp").src = "image/meter_igoff.jpg";
+    document.getElementById("setVelocityBase").style.display = "none";
+    document.getElementById("setVelocity").style.display = "none";
+})();
 
 // パワースイッチONでメータ表示
 function ignitionON(){
     const elements = document.getElementsByName("ignition");
-    if(elements[0].checked){
+    ig = elements[0].checked;
+    if(ig){
         document.getElementById("meterDisp").src = "image/meter_base.jpg";
         document.getElementById("setVelocityBase").style.display = "block";
         document.getElementById("setVelocity").style.display = "block";
@@ -16,6 +23,8 @@ function ignitionON(){
         document.getElementById("setVelocity").style.display = "none";
     }
 }
+
+
 
 // +RESスイッチ押下
 function pushRES(){
@@ -38,4 +47,17 @@ function pushRES(){
 function pushSET(){
     v_set -= 1;
     document.getElementById("setVelocity").textContent = v_set;
+}
+
+
+window.onload = function(){
+    var $res = document.getElementById("res");
+    $res.onmousedown = fonction($event) 
+    {
+        if($event.button == 0){
+            console.log("test");
+            v_set -= 1;
+            document.getElementById("setVelocity").textContent = v_set;
+        }
+    }
 }
